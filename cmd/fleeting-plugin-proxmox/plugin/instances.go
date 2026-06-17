8b0ce4e3dc8e0b1130eb07ee37ecc4c3316309f8
Phenix66 <34311559+Phenix66@users.noreply.github.com>
2026-06-17 07:28:24 -0400

    Revert "Fixes silly Go syntax error"
    This reverts commit 715a6837358283239fd6a3ce55b02ca6f89882cd.

--- cmd/fleeting-plugin-proxmox/plugin/instances.go
+++ cmd/fleeting-plugin-proxmox/plugin/instances.go
@@ -122,7 +122,7 @@ func (ig *InstanceGroup) getTemplateCloneOptions(template *proxmox.VirtualMachin
 		return nil, ErrCloneVMWithoutConfiguredStorage
 	}
 
-	if ig.Settings.UseLinkedClones || (template.Template == true && ig.Settings.Storage == "") {
+	if (template.Template && ig.Settings.Storage == "") || ig.Settings.UseLinkedClones {
 		cloneOptions.Full = 0
 	}
 
